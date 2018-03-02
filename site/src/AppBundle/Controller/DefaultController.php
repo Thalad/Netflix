7d949058995775e792bd4f49be806e926b44450a
ttalbi <ttalbiadil@gmail.com>
2018-03-02 16:19:24 +0100

    serie

--- site/src/AppBundle/Controller/DefaultController.php
+++ site/src/AppBundle/Controller/DefaultController.php
@@ -17,15 +17,14 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     */
+     *
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
-    }
+    }*/
 
     /**
      * @Route("/home/films", name="films_list")
@@ -171,7 +170,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/home", name="home_page")
+     * @Route("/", name="home_page")
      */
 
     public function homePage()
